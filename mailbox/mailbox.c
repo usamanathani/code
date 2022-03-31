26d88896b51c9e55a596bc448a2d34fcf637e7a9
m.usama.nathani@gmail.com <m.usama.nathani@gmail.com>
2022-03-30 19:52:56 -0700

    Changed the way threads are created. Previously threads were created in loops. Now threads are only started once and loop inside. Threads keep looping until mailbox is full. Mutex placed in send to avoid reuse of resources. Head Tail approach to Buffer to imitate FIFO (First in First Out)

--- mailbox/mailbox.c
+++ mailbox/mailbox.c
@@ -1,14 +1,3 @@
-/*
-
-1. Thread 1 and Thread 2 Send Messages to Mailbox (Buffer)
-2. Thread 1 Send Message containing "Event1" every 100ms, Thread 2, "Event2", every 110ms
-3. Thread 3 receives Messages from Mailbox. If Event 1 is received, it is printed with its timestamp. 
-4, Thread 3 acknowledges Thread1 / Thread2 Msg Processed so next event can be sent. 
-
-
-*/
-
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
@@ -16,222 +5,149 @@
 #include <unistd.h>
 #include <sys/time.h>
 #include <time.h>
+#include <semaphore.h>
 
-clock_t start_time;
-int addr  = 0;
-pthread_mutex_t lock; // add lock to the mailbox. 
-
-
-
-void delay(int milli)
-{
-	long pause;
-	clock_t now, then;
-	pause = milli * (CLOCKS_PER_SEC/1000);
-	now = then = clock ();
-	while((now-then)<pause)
-		now=clock();
-}
-//usleep better choice
-
-void get_time(int choice) 
-{
-	clock_t now;
-	double diff;
-
-	now = clock();
-	diff = (now-start_time/CLOCKS_PER_SEC)/1000;
-	if(choice==1)
-	printf("[Time: %.2f ms]\n", diff);
-	else
-	printf("\t[Time: %.2f ms]\n", diff);
-}	
-
+pthread_mutex_t lock;
+int mailbox_size = 100;
 
 struct msg 
 {
 	int event;
-	pthread_t thread;
+	int ack; 
 	int val;
+	int id;
 };
 
+ 
 struct mailbox 
 {
-	struct msg buffer[32]; // Mailbox message buffer
-	int ack1;  // to handle two threads simultaneously
+	struct msg buffer[100]; // Mailbox message buffer
+	int total; // total messages sent
+	int count;
+	struct msg data;
+	int head;
+	int tail;
+	int ack1;
 	int ack2;
-	int count; // number of messages in mailbox
-	int total;
 
 };
 
-struct mail_data
+
+void msg_send(struct mailbox *mbx_s)
 {	
-	struct msg msg_d;
-	struct mailbox mailbox_d;
-};
-
-
-void *msg_send(void *md)
-{
-
-
+	
 	pthread_mutex_lock(&lock);
-	struct mail_data *args = (struct mail_data *)md;
-
-	if(args->mailbox_d.count > 32) //
-		delay(10);
-	else
-	{
-		args->mailbox_d.count = args->mailbox_d.count + 1;
-		args->mailbox_d.total = args->mailbox_d.total + 1;
-		args->mailbox_d.ack1 = 0;
-		args->mailbox_d.ack2 = 0;
-		args->mailbox_d.buffer[addr] = args->msg_d;
-		addr = addr + 1;
-		printf("Sent Msg!     Event:%d Value:%d  (Mailbx Size: %d | Msgs Sent: %d)", args->msg_d.event,args->msg_d.val,args->mailbox_d.count, args->mailbox_d.total);
-		get_time(2);
-	}
-	
+	struct mailbox *mailbox_s = (struct mailbox *)mbx_s;
+	mailbox_s->buffer[mailbox_s->tail] = mailbox_s->data;
+	mailbox_s->total = mailbox_s->total + 1;
+	mailbox_s->count = mailbox_s->count + 1;
+	printf("Message Sent! Event:%d Val:%d ID:%d Total:%d\n", mailbox_s->data.event,mailbox_s->data.val,mailbox_s->data.id, mailbox_s->total);
+	mailbox_s->tail = mailbox_s->tail + 1;
 	pthread_mutex_unlock(&lock);
-}
 
 
-void *msg_rec(void *md)
-{	
 
-	pthread_mutex_lock(&lock);
-	struct mail_data *args = (struct mail_data *)md;
-	
-	
-	while(args->mailbox_d.count != 0) // check mailbox is not empty
-	{		
-			args->mailbox_d.count = args->mailbox_d.count - 1;
-			args->msg_d = args->mailbox_d.buffer[addr-1];
-			addr = addr - 1;
+}	
 
-			printf("Recieved Msg! Event:%d Value:%d  (Mailbx Size: %d | Msgs Sent: %d)", args->msg_d.event,args->msg_d.val,args->mailbox_d.count, args->mailbox_d.total);
-			get_time(2);
-
-
-			if(args->msg_d.event == 1)
-				args->mailbox_d.ack1 == 1;
-			else
-				args->mailbox_d.ack2 == 1;
-	}
-	pthread_mutex_unlock(&lock);
-}
-
-void rec_event(struct mail_data *maildata)
+void *thread1(void *th)
 {
-	int rc;
-	struct msg data;
-	pthread_t thread3;
 
+	struct mailbox *mailbox_t = (struct mailbox *)th;
+	if(mailbox_t->ack1 == 1)
+	{
+		usleep(100000);
+		struct msg data_send;
 
-	if(pthread_mutex_init(&lock, NULL)!=0) // Initializing lock
-		printf(" \n Mutex Init Failed! \n");
+		data_send.event = 1;
+		data_send.ack = 0;
+		data_send.val = rand() % 100 + 1;
+		data_send.id = mailbox_t->total + 1;
+		mailbox_t->data = data_send;
+		mailbox_t->ack1 = 0;
+		msg_send(mailbox_t);
 
-	else
-		rc = pthread_create(&thread3, NULL, msg_rec, maildata); 
-		
-		if(pthread_join(thread3, NULL) != 0)
-			printf("Pthread join fail \n");
-
+	}
 }
 
-
-
-void create_event(struct mail_data *maildata, int event_type, int ms_delay) //msg_send must be ptr
+void *thread2(void *th)
 {
-	int rc;
-	struct msg data;
-	int ack;
 
-	if(maildata->mailbox_d.total != 0)
+	struct mailbox *mailbox_t = (struct mailbox *)th;
+	if(mailbox_t->ack2 == 1)
 	{
-		if(event_type == 1)
-			ack = maildata->mailbox_d.ack1;
-		else
-			ack = maildata->mailbox_d.ack2;		
+		usleep(110000);
+		struct msg data_send;
+
+		data_send.event = 2;
+		data_send.ack = 0;
+		data_send.val = rand() % 100 + 1;
+		data_send.id = mailbox_t->total + 1;
+		mailbox_t->data = data_send;
+		mailbox_t->ack2 = 0;
+		msg_send(mailbox_t);
 	}
-	else
-		ack = 1; // Initializing. 
-		maildata->mailbox_d.ack1 = 1;
-		maildata->mailbox_d.ack2 = 1;
+}
 
 
-	if(ack)
-	{
-		
-		data.event = event_type;
-		data.val = rand() % 100 + 1;
-		
-		delay(ms_delay);
-		maildata->msg_d = data;
 
-		if(pthread_mutex_init(&lock, NULL)!=0)
-			printf(" \n Mutex Init Failed! \n");
-		else
-			rc = pthread_create(&maildata->msg_d.thread, NULL, msg_send, maildata);
 
-	}
-
+void rec_event(struct msg data_r)
+{
+	printf("Recieved Event! Event:%d Val:%d ID:%d\n", data_r.event, data_r.val, data_r.id);
 
 }
+
+void *thread3(void *th)
+{
+	struct mailbox *mailbox_r = (struct mailbox *)th;
+	struct msg data_rec;
+
+	
+	data_rec = mailbox_r->buffer[mailbox_r->head];
+	
+	mailbox_r->head = mailbox_r->head + 1;
+	mailbox_r->count = mailbox_r->count - 1;
+
+	if(data_rec.event == 1)
+		mailbox_r->ack1 = 1;
+	else
+		mailbox_r->ack2 = 1;
+
+	rec_event(data_rec);
+
+}
+
 
 
 void main()
-{		
-	start_time = clock();
-	
-	struct mail_data *maildata = (struct mail_data *)malloc(sizeof(struct mail_data));
-	
-	maildata->mailbox_d.count = 0;
-	maildata->mailbox_d.total = 0;
+{
+	pthread_t s_thread1, s_thread2;; // send thread 1, thread 2
+	pthread_t r_thread3;
+	int rc;
 	srand(time(NULL));
 
-	get_time(1); //Start
-	for(int i=0; i<40; i++)
+	struct mailbox *mailbox_m = (struct mailbox *)malloc(sizeof(struct mailbox));
+	mailbox_m->tail = 0;
+	mailbox_m->head = 0;
+	mailbox_m->total = 0;
+	mailbox_m->count = 0;
+	mailbox_m->ack1 = 1;
+	mailbox_m->ack2 = 1;
+	pthread_mutex_init(&lock, NULL);
+	
+	while(mailbox_m->count <mailbox_size)
 	{	
+		rc = pthread_create(&s_thread1, NULL, thread1, mailbox_m);
+		rc = pthread_create(&s_thread2, NULL, thread2, mailbox_m);
+		rc = pthread_join(s_thread1,NULL);
+		rc = pthread_join(s_thread2,NULL);	
+		rc = pthread_create(&r_thread3,NULL,thread3,mailbox_m);
+		rc = pthread_join(r_thread3, NULL);
 
-		create_event(maildata, 1, 100);
-		pthread_join(maildata->msg_d.thread, NULL);
-		rec_event(maildata);
-		create_event(maildata, 2, 10);
-		rec_event(maildata);
-		pthread_join(maildata->msg_d.thread, NULL);
-		
+		if(mailbox_m->head == mailbox_size)
+			mailbox_m->head = 0;
+		if(mailbox_m->tail == mailbox_size)
+			mailbox_m->tail = 0;
 	}
-	get_time(1); // End
-	
-	//printf("Final Address of addr: %d\n", addr);
-	
-	// printf("Buffer check! \n");
-	// for(int i=0; i<20; i++)
-	// {
-	// 	printf("Buffer[%d] = Event%d\n",i,maildata->mailbox_d.buffer[i].event);
-	// }
-}	
-
-
-
-/*
-
-void *display1 (void *args)
-{
-	int *val = (int *)args;
-	printf("Value is %d \n", *val);
 
 }
-
-
-void *display (void *args)
-{
-	char *val = (char *)args;
-	printf("Value is %s \n", val);
-
-}
-
-*/
-
