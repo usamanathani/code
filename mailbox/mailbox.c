9f9bba9a3d1c1265b0fc3c3d399b7466bfcf0824
m.usama.nathani@gmail.com <m.usama.nathani@gmail.com>
2022-03-30 19:40:15 -0700

    Added 2 events with delay and recieve event. Currently checks if mailbox is busy and also added ack. Using addr as a global variable, we utilize the buffer inside the mailbox as LIFO (Last In First Out)

--- mailbox/mailbox.c
+++ mailbox/mailbox.c
@@ -1,3 +1,13 @@
+/*
+
+1. Thread 1 and Thread 2 Send Messages to Mailbox (Buffer)
+2. Thread 1 Send Message containing "Event1" every 100ms, Thread 2, "Event2", every 110ms
+3. Thread 3 receives Messages from Mailbox. If Event 1 is received, it is printed with its timestamp. 
+4. If Thread 1 executes, it will not send another message until ack, which is when the thread Event is printed with timestamp. 
+
+*/
+
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
@@ -5,6 +15,9 @@
 #include <unistd.h>
 #include <sys/time.h>
 #include <time.h>
+
+clock_t start_time; // Start time of Program to baseline get_time()
+int addr = 0;  // Global buffer counter
 
 
 void delay(int milli)
@@ -18,27 +31,158 @@ void delay(int milli)
 }
 
 
-void get_time(clock_t start) 
+void get_time(int choice) 
 {
 	clock_t now;
 	double diff;
-	start = clock();
 
 	now = clock();
-	diff = (now-start/CLOCKS_PER_SEC)/1000;
-	printf("Time: %.2f ms\n", diff);
-	
+	diff = (now-start_time/CLOCKS_PER_SEC)/1000;
+	if(choice==1)
+	printf("[Time: %.2f ms]\n", diff);
+	else
+	printf("\t[Time: %.2f ms]\n", diff);
+}	
+
+
+struct msg 
+{
+	int event;
+	pthread_t thread;
+	int ack;
+};
+
+struct mailbox 
+{
+	struct msg buffer[32]; // 0 --> 31
+	int ack1;  // to handle two threads simultaneously
+	int ack2;
+	int busy;
+	int m_count; // mailbox count 
+};
+
+struct mail_data
+{	
+	struct msg *msg_d;
+	struct mailbox *mailbox_d;
+};
+
+
+void *msg_send(void *md)
+{
+	struct mail_data *args = (struct mail_data *)md;
+
+	struct msg msg_s = args->msg_d;
+	struct mailbox mb_s = args-> mailbox_d;
+	mb_s.buffer[addr] = args->msg_d;
+	addr = addr + 1;
+
+	printf("Sent Message! (Event%d)", msg_s.event);
+	get_time(2);
+	printf("Mailbox count: %d \n", mb_s.m_count);
+
 }
 
-struct msg {
-	pthread_t thread;
-	char* str;
-};
+void event1(struct mail_data maildata)
+{
+	printf("Event 1 has been processed");
+	get_time(2);
+	maildata.mailbox_d.ack1 = 1;
+}
 
-struct msg1 {
-	pthread_t thread;
-	int value; 
-};
+void event2(struct mail_data maildata)
+{
+	printf("Event 2 has been processed");
+	get_time(2);
+	maildata.mailbox_d.ack2 = 1;
+}
+
+void *msg_rec(void *md)
+{
+	struct mail_data *args = (struct mail_data *)md;
+	struct msg msg_s = args->msg_d;
+	struct mailbox mb_s = args-> mailbox_d;
+
+	int output_event;
+
+	output_event = mb_s.buffer[addr].event;
+	addr = addr - 1;
+
+	if(mb_s.busy != 1)
+	{
+		if(output_event == 1)
+		{
+			event1(*args);
+			mb_s.busy = 1;
+			printf("Mailbox count: %d \n", args->mailbox_d.m_count);
+		}
+		else if(output_event == 2)
+		{
+			event2(*args);
+			mb_s.busy = 1;
+			printf("Mailbox count: %d \n", mb_s.m_count);
+		}
+	}
+}
+
+void create_event(struct mail_data *maildata, int event_type) 
+{
+	int rc;
+	maildata.msg_d.event = event_type;
+	maildata.msg_d.ack = 0;
+	maildata.mailbox_d.buffer[addr] = maildata.msg_d; // Need global variable buffer counter
+	
+
+	rc = pthread_create(maildata.msg_d.thread, NULL, msg_send, (void *) &maildata);
+
+}
+
+
+
+void rec_event(struct mail_data maildata)
+{	
+	if(maildata.mailbox_d.m_count != 0 )
+	{
+		int rc;
+
+		rc = pthread_create(&maildata.msg_d.thread, NULL, msg_rec, (void *) &maildata);
+	}
+}
+
+
+
+void main()
+{		
+	
+	struct mail_data *mdx = malloc(sizeof struct maildata);
+	mdx->mailbox_d.m_count = 0; // Initializing number of messages in mailbox.
+	
+	
+	get_time(1); //Start
+	for(int i=0; i<10; i++)
+	{	
+		delay(100);
+		create_event(mdx, 1);
+		pthread_join(mdx->msg_d.thread, NULL);
+		delay(10);
+		create_event(mdx, 2);
+		pthread_join(mdx->msg_d.thread, NULL);
+			
+		rec_event(*mdx);
+	}
+
+	get_time(1); // End
+	
+	
+	
+	free(mdx);
+}
+
+
+
+
+
+/*
 
 void *display1 (void *args)
 {
@@ -55,37 +199,4 @@ void *display (void *args)
 
 }
 
-
-
-
-void main()
-{		
-	clock_t start;
-	start = clock();
-	int rc;
-	struct msg event;
-	struct msg1 event2;
-
-	event.str = "Event1";
-
-	event2.value = 10;
-
-
-
-	for(int i =0; i<5; i++) 
-	{
-		get_time(start);
-		delay(200);
-		rc = pthread_create(&event.thread, NULL, display, (void *)event.str);
-		
-		rc = pthread_create(&event2.thread, NULL, display1, (void *)&event2.value); 
-
-	}
-
-	pthread_exit(NULL);
-
-
-
-
-}
- 
+*/
